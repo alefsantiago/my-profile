d8535965098b507549ec8bc5e618c4e22afc1884
alefsantiago <alefzell@gmail.com>
2024-04-16 15:42:49 -0300

    modificando imagens e adicionando js

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 function toggleMode () {
-    const html = document.documentElement   
+    const html = document.documentElement 
 
+    /* versão if e else
+      if (html.classList.contains('light')) {
+        html.classList.remove('light')
+      }  else {
+        html.classList.add('light')
+      }
+    
+    */
+
+    // versão simplicada
     html.classList.toggle('light')
+
+    const img = document.querySelector("#profile img")
+
+    if(html.classList.contains('light')) {
+        img.setAttribute("src", "./assets/avatar-light.png")
+    } else {
+        img.setAttribute("src", "./assets/avatar.png")
+    }
 }
